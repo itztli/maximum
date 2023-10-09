4fa3f79e62b980fd00c06870fc675ae87f288467
Victor De la Luz <vdelaluz@enesmorelia.unam.mx>
2023-10-09 12:56:00 -0600

    deleting hardcode

--- minimum.py
+++ minimum.py
@@ -4,9 +4,11 @@
 # By: vdelaluz@enesmorelia.unam.mx
 # LICENSE GNU/GPL
 
+import sys
+
 data = [1.0, 3.14, 6.2, 0.1, 5.3]
 
-minimum = 99999.99
+minimum = sys.float_info.max
 
 for x in data:
     if x < minimum:
