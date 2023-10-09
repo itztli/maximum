8eb3b68f17c820c5bd9bbbc11d2fed8eb5aa0407
Victor De la Luz <vdelaluz@enesmorelia.unam.mx>
2023-10-09 12:36:50 -0600

    Maximum algorithm

---
+++ maximum.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Maximum algorithm
+# By: vdelaluz@enesmorelia.unam.mx
+# LICENSE GNU/GPL
+
+data = [1.0, 3.14, 6.2, 0.1, 5.3]
+
+maximum = -99999.99
+
+for x in data:
+    if x > maximum:
+        maximum = x
+    #print(x)
+
+print(maximum)
